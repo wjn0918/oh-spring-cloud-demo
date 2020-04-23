df92c4e7a58fa6ea1dd041c18224f49d5174eb60
wjn0918 <wjn_0918@163.com>
2020-04-23 18:25:46 +0800

    服务提供者

---
+++ spring-cloud-service-provider/src/main/java/com/example/hello/spring/cloud/service/provider/ServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello.spring.cloud.service.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderApplication.class,args);
+    }
+}
