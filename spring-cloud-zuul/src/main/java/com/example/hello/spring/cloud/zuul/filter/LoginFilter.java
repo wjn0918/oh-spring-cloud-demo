2515636f0ed6b24eaf6107da195073906b247b07
wjn0918 <wjn_0918@163.com>
2020-04-24 15:21:37 +0800

    添加请求过滤功能

---
+++ spring-cloud-zuul/src/main/java/com/example/hello/spring/cloud/zuul/filter/LoginFilter.java
@@ -0,0 +1,47 @@
+package com.example.hello.spring.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String token = request.getParameter("token");
+        if(token == null){
+            currentContext.setSendZuulResponse(false);
+            currentContext.setResponseStatusCode(401);
+            try {
+                HttpServletResponse response = currentContext.getResponse();
+                response.setContentType("text/html;charset=UTF-8");
+                currentContext.getResponse().getWriter().write("非法请求");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
